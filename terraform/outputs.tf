b775803547ec90c05c3b8d00339bbc8ea77e25c8
Votok23 <votok23@gmail.com>
2025-08-26 15:13:07 +0700

    commit8

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vm1_public_ip" {
-  value = yandex_compute_instance.vm1.network_interface.0.nat_ip_address
-}
-
-output "vm2_internal_ip" {
-  value = yandex_compute_instance.vm2.network_interface.0.ip_address
-}
